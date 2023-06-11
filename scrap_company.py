cb925c0e2f549fffca43ca3c20f4fe6500276af8
Juan Carlos Alvarez <juancarlosa@clientbook.com>
2023-06-10 23:07:54 -0600

    using firefox to avoid cloudflare verification

--- scrap_company.py
+++ scrap_company.py
@@ -3,17 +3,18 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.firefox.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.g2.com/products/bitly/reviews")
     page.wait_for_url("https://www.g2.com/products/bitly/reviews")
-    # page.locator(".paper > a").first.click()
-    page.locator(".hide-for-custom-profile").locator(".link .x-toggle-visible-initialized").locator("a").click()
-    base_company_locator = page.locator(".paper .paper--box")
+    # page.get_by_text("Show More").nth(2).click()
+    # page.locator(".hide-for-custom-profile").locator(".link .x-toggle-visible-initialized").locator("a").click()
+    base_company_locator = page.locator(".hide-for-custom-profile .paper--nestable").nth(2)
 
     # name = base_company_locator.get_by_text("Seller").locator(".ml-1").inner_text()
-    website = base_company_locator.get_by_text("Company Website").locator("a").inner_text()
+    name = page.locator(".product-head__title").locator("a").first.inner_text()
+    website = base_company_locator.get_by_text("Website").inner_text()
     # year_fundation = base_company_locator.get_by_text("Year Founded").inner_text()
     # location = base_company_locator.get_by_text("HQ Location").inner_text()
     #  page.locator(".product-head__logo").click()
@@ -29,6 +30,7 @@ def run(playwright: Playwright) -> None:
     #     "location": location
     # }
     # print(json.dumps(company))
+    print(name)
     print(website)
 
 
