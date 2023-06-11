65cb30ba2654ec8ac9104c160e9d34ffdb157536
Juan Carlos Alvarez <juancarlosa@clientbook.com>
2023-06-11 13:48:50 -0600

    using xpath to locate elements.

--- scrap_company.py
+++ scrap_company.py
@@ -1,4 +1,5 @@
 import json
+import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
@@ -8,30 +9,23 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://www.g2.com/products/bitly/reviews")
     page.wait_for_url("https://www.g2.com/products/bitly/reviews")
-    # page.get_by_text("Show More").nth(2).click()
-    # page.locator(".hide-for-custom-profile").locator(".link .x-toggle-visible-initialized").locator("a").click()
-    base_company_locator = page.locator(".hide-for-custom-profile .paper--nestable").nth(2)
+    
+    website = page.locator('//*[@id="leads-sticky-top"]/div/div[1]/div[3]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div/div/a').inner_text()
+    description = page.locator('//*[@id="leads-sticky-top"]/div/div[1]/div[3]/div[1]/div[1]/div[2]/div/p').inner_text()
+    rating = page.locator('//*[@id="products-dropdown"]/div[1]/div[1]/span[1]').inner_text()
+    # Click 'show more' to see details
+    page.locator('//*[@id="leads-sticky-top"]/div/div[1]/div[3]/div/div[1]/div[2]/a').first.click()
+    detail = page.locator('//*[@id="leads-sticky-top"]/div/div[1]/div[3]/div[1]/div/div[2]/div[2]').inner_text()
 
-    # name = base_company_locator.get_by_text("Seller").locator(".ml-1").inner_text()
-    name = page.locator(".product-head__title").locator("a").first.inner_text()
-    website = base_company_locator.get_by_text("Website").inner_text()
-    # year_fundation = base_company_locator.get_by_text("Year Founded").inner_text()
-    # location = base_company_locator.get_by_text("HQ Location").inner_text()
-    #  page.locator(".product-head__logo").click()
     page.close()
 
     # ---------------------
     context.close()
     browser.close()
-    # company ={
-    #     "name": name,
-    #     "website": website,
-    #     "year_fundation": year_fundation,
-    #     "location": location
-    # }
-    # print(json.dumps(company))
-    print(name)
     print(website)
+    print(description)
+    print(rating)
+    print(detail)
 
 
 with sync_playwright() as playwright:
